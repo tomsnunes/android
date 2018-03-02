68fb83786223e1f5d9404882d171ec22d81fbc9c
tobiasKaminsky <tobias@kaminsky.me>
2018-03-02 12:08:09 +0100

    Add support for integration tests
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ scripts/uploadIT.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+URL=https://nextcloud.kaminsky.me/remote.php/webdav/integrationTests
+ID=$3
+USER=$1
+PASS=$2
+
+cd build/reports/androidTests/connected/flavors/GPLAY
+
+find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$ID/$(echo {} | sed s#\./##) \;
+find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$ID/$(echo {} | sed s#\./##) --upload-file {} \;
+
+echo "Uploaded failing tests to https://nextcloud.kaminsky.me/index.php/s/XqY52jBr9ZYfDiz -> $ID" 
