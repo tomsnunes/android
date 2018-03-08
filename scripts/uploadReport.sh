ebecdd25ba31471d22b9f6f0d75feef427e39813
tobiaskaminsky <tobias@kaminsky.me>
2018-03-08 09:45:28 +0100

    upload report for failing unit or it tests, after that exit
    Signed-off-by: tobiaskaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -4,10 +4,16 @@ URL=https://nextcloud.kaminsky.me/remote.php/webdav/integrationTests
 ID=$3
 USER=$1
 PASS=$2
+TYPE=$4
 
-cd build/reports/androidTests/connected/flavors/GPLAY
+if [ $TYPE = "IT" ]; then
+    cd build/reports/androidTests/connected/flavors/GPLAY
+else 
+    cd build/reports/tests/testGplayDebugUnitTest
+fi
 
 find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$ID/$(echo {} | sed s#\./##) \;
 find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$ID/$(echo {} | sed s#\./##) --upload-file {} \;
 
 echo "Uploaded failing tests to https://nextcloud.kaminsky.me/index.php/s/XqY52jBr9ZYfDiz -> $ID" 
+exit 1
